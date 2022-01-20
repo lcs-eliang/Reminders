5dc7337252acc4a3389e021668c13e0bddee8b26
Emily Liang <eliang@lcs.on.ca>
2022-01-20 18:43:52 -0500

    Finished Reminders App.

--- Shared/Views/AddTask.swift
+++ Shared/Views/AddTask.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct AddTask: View {
     
+    // Get a reference to the store of task (TaskStore)
+    @ObservedObject var store: TaskStore
+    
     // Details of the new task
     @State private var description = ""
     @State private var priority = TaskPriority.low
@@ -43,6 +46,11 @@ struct AddTask: View {
     
     func saveTask() {
         
+        // Add the task to the list of tasks
+        store.tasks.append(Task(description: description,
+                                priority: priority,
+                                completed: false))
+        
         // Dismis this view
         showing = false
         
@@ -52,6 +60,6 @@ struct AddTask: View {
 
 struct AddTask_Previews: PreviewProvider {
     static var previews: some View {
-        AddTask(showing: .constant(true))
+        AddTask(store: testStore, showing: .constant(true))
     }
 }
